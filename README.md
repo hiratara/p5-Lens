6ec5ca353ead6e5a2d5699dda587ad3684f570cb
Masahiro Honma <hiratara@cpan.org>
2014-08-24 11:59:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Lens - Blah blah blah
+
+# SYNOPSIS
+
+    use Lens;
+
+# DESCRIPTION
+
+Lens is
+
+# AUTHOR
+
+Masahiro Honma <hiratara@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2014- Masahiro Honma
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Lens.pm
@@ -0,0 +1,39 @@
+package Lens;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Lens - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Lens;
+
+=head1 DESCRIPTION
+
+Lens is
+
+=head1 AUTHOR
+
+Masahiro Honma E<lt>hiratara@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Masahiro Honma
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
